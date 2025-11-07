242de935281b2310fd6c7d6831bd84c2fbc856ae
Lust <104316481+ArianLust@users.noreply.github.com>
2025-11-07 16:02:41 +0100

    (Re)added Version separation
    Added version separation to AppVersion during connection so the game's version (for mods for example) doesn't have to become a mess (and possibly for the vanilla game too like 1.7 did)

--- Assets/Scripts/Networking/NetworkHandler.cs
+++ Assets/Scripts/Networking/NetworkHandler.cs
@@ -154,7 +154,7 @@ namespace NSMB.Networking {
             try {
                 await Client.ConnectUsingSettingsAsync(new AppSettings {
                     AppIdQuantum = "6b4b72d0-57c3-4991-96c1-f3f36f9548e5",
-                    AppVersion = GameVersion.Parse(Application.version).ToStringIgnoreHotfix(),
+                    AppVersion = GameVersion.Parse(Application.version).ToStringIgnoreHotfix() + "", //version seperation
                     EnableLobbyStatistics = true,
                     AuthMode = AuthModeOption.Auth,
                     FixedRegion = region,
